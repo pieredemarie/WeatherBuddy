ef12cfabce30fa36afbca4d5c553039f54d021f7
Khachatryan Artem <65036324+pieredemarie@users.noreply.github.com>
2026-08-13 11:30:20 +0300

    added database layer

--- scheduler-service/internal/repository/postgres/postgres.go
+++ scheduler-service/internal/repository/postgres/postgres.go
@@ -1,1 +1,21 @@
 package postgres
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+func NewDB(ctx context.Context, dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.PingContext(ctx); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("ping postgres: %w", err)
+	}
+
+	return db, nil
+}
--- scheduler-service/internal/service/scheduler.go
+++ scheduler-service/internal/service/scheduler.go
@@ -1,14 +1,1 @@
 package service
-
-import (
-	"WeatherBuddy/registration-service/internal/model"
-	"context"
-)
-
-type DueUsersRepo interface {
-	DueUsers(ctx context.Context) ([]model.User, error)
-}
-
-type JobPublisher interface {
-	Publish(ctx, context.Context, j)
-}
